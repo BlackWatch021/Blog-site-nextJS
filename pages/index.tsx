d67ec892d992ea8e0b19e3a948430bb8a2fb7e31
Himanshu Negi <himanshu.21negi@gmail.com>
2023-03-17 20:30:00 +0530

    Posts added in sanity and additional stuff in the site

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,15 @@ import Banner from "../components/Banner";
 import BannerBottom from "../components/BannerBottom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { sanityClient, urlFor } from "../sanity";
+import { Post } from "../types";
+import Image from "next/image";
 
-export default function Home() {
+interface Props {
+  posts: [Post];
+}
+
+export default function Home({ posts }: Props) {
   return (
     <div>
       <Head>
@@ -25,7 +32,17 @@ export default function Home() {
         </div>
         {/* ============ Banner-Bottom End here ======= */}
         {/* ============ Post Part Start here ========= */}
-        <div className="max-w-7xl mx-auto py-20 px-4">Posts will go here</div>
+        <div className="max-w-7xl mx-auto py-20 px-4">
+          {posts.map((el) => (
+            <div>
+              <Image
+                width={380}
+                height={350}
+                src={urlFor(el.mainImage).url()}
+              />
+            </div>
+          ))}
+        </div>
         {/* ============ Post Part End here =========== */}
         {/* ============ Footer Start here============= */}
         <Footer />
@@ -34,3 +51,22 @@ export default function Home() {
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  const query = `*[_type=="post"]{
+    _id,
+      title,
+      author->{
+        name,image
+      },
+      description,
+      mainImage,
+      slug
+  }`;
+  const posts = await sanityClient.fetch(query);
+  return {
+    props: { posts },
+  };
+};
+
+//32:00
---
+++ types.ts
@@ -0,0 +1,16 @@
+export interface Post {
+  _id: string;
+  publishedAt: string;
+  title: string;
+  author: {
+    name: string;
+    image: string;
+  };
+  //   comments: Comment[];
+  description: string;
+  mainImage: {
+    asset: { url: string };
+  };
+  slug: { current: string };
+  body: [object];
+}
