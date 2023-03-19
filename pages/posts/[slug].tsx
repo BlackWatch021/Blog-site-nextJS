aef17a3cda965b385cb3a7ce06437822b14178f2
Himanshu Negi <himanshu.21negi@gmail.com>
2023-03-19 18:27:47 +0530

    Error #3 resolved

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -7,7 +7,7 @@ import Header from "../../components/Header";
 import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "../../types";
 import { useForm, SubmitHandler } from "react-hook-form";
-import comment from "../../personal-blog/schemas/comment";
+// import comment from "../../personal-blog/schemas/comment";
 import { useSession } from "next-auth/react";
 
 interface Props {
